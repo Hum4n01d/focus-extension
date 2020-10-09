f8c6965485bf32dc7da596208515d67b2362dce1
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2020-10-09 16:12:38 -0700

    Redesign, add checkbox

--- src/content/content.js
+++ src/content/content.js
@@ -1,14 +1,17 @@
-chrome.storage.sync.get(['blacklistGlobs'], ({ blacklistGlobs = [] }) => {
+chrome.storage.sync.get(['blacklistGlobs', 'isEnabled'], ({ blacklistGlobs = [], isEnabled = true }) => {
+  console.log(isEnabled);
+  if (!isEnabled) return
+
   blacklistGlobs.forEach(async glob => {
     if (!glob) return
 
     const pattern = new RegExp(glob)
 
     if (pattern.test(window.location.href)) {
-      const resp = await fetch(chrome.runtime.getURL('overlay.html'))
-      const overlayHTML = await resp.text()
+      const resp = await fetch(chrome.runtime.getURL('src/content/blocked.html'))
+      const blockedHTML = await resp.text()
 
-      document.body.insertAdjacentHTML('beforebegin', overlayHTML)
+      document.write(blockedHTML)
     }
   })
 })
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,14 +1,19 @@
-import './popup.css'
-
 const blacklistEl = document.querySelector('#blacklist')
+const checkboxEl = document.querySelector('#checkbox')
 
 window.addEventListener('load', (event) => {
-  chrome.storage.sync.get(['blacklistGlobs'], ({ blacklistGlobs = [] }) => {
+  chrome.storage.sync.get(['blacklistGlobs', 'isEnabled'], ({ blacklistGlobs = [], isEnabled = true }) => {
     blacklistEl.value = blacklistGlobs.join('\n') || ''
+    checkboxEl.checked = isEnabled
   })
 })
 
 blacklistEl.addEventListener('input', () => {
   const blacklistGlobs = blacklistEl.value.split('\n')
   chrome.storage.sync.set({ blacklistGlobs })
+})
+
+checkboxEl.addEventListener('click', () => {
+  const isEnabled = checkboxEl.checked
+  chrome.storage.sync.set({ isEnabled })
 })
